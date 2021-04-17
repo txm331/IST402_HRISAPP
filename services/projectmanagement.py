34432ef0bdfca5a3cd5546a6530ce54bda1adb22
Daniel <dfc5292@psu.edu>
2021-04-16 21:12:18 -0400

    updated code

--- services/projectmanagement.py
+++ services/projectmanagement.py
@@ -1,17 +1,47 @@
-from flask import Flask, render_template
- 
-
+from flask import Flask, jsonify, make_response
+import json
+import os
+import simplejson as json
+#projectmanagement
 app = Flask(__name__)
 
-headings = ("Task","Task Assigned","Task Status")
-data = (
-    ("Estimate budget", "Timothy McCreesh", "Completed"),
-    ("Web page designs", "Enci Zou", "In Progress"),
-    ("Project Documentation", "Daniel Chao", "In Progress"),
-    ("Program web pages", "Druvil Patel", "In Progress"),
-)
+database_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+print(database_path)
 
-@app.route("/")
-def home():
-    
-    return render_template('tasktable.html')
+with open("{}/static/projectmanagement.json".format(database_path), "r") as f:
+    acc = json.load(f)
+
+@app.route('/', methods=['GET'])
+def hello():
+    ''' Greet the user '''
+
+    return "Hey! The service is up, how about doing something useful"
+
+@app.route('/projectmanagement', methods=['GET'])
+def projectmangement():
+    ''' Displays all the lists '''
+    resp = make_response(json.dumps(acc, sort_keys=True, indent=4))
+    resp.headers['Content-Type']="application/json"
+    return resp
+
+@app.route('/projectmanagement/<username>', methods=['GET'])
+def projectmangement_data(username):
+    ''' Returns info about a specific user '''
+
+    if username not in acc:
+        return "Not found"
+
+    return jsonify(acc[username])
+
+@app.route('/projectmanagement/<username>/lists', methods=['GET'])
+def projectmangement_lists(username):
+    ''' Get lists based on username '''
+
+    try:
+        req = requests.get("http://127.0.0.1:5001/lists/{}".format(username))
+    except requests.exceptions.ConnectionError:
+        return "Service unavailable"
+    return req.text
+
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
